7a01781b789e480636cb1a05b3c93581d29d3272
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-03-26 19:47:24 +0200

    created MyView and SingleComponent

---
+++ appVue3/src/components/myComponents/SingleComponent.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {};
+</script>
+
+<template>
+    <div>
+        <h3>My Single Component for Femcoders Malaga</h3>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    h3 {
+        color: yellow;
+    }
+</style>
---
+++ appVue3/src/views/MyView.vue
@@ -0,0 +1,21 @@
+<script>
+    import SingleComponent from '../components/myComponents/SingleComponent.vue'
+
+    export default {components: { SingleComponent } };
+</script>
+
+<template>
+    <div>
+        <h1>My Single Page from Femcoders</h1>
+        <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi provident dolor, quos est sapiente quibusdam quidem modi minus esse? Fuga et sed saepe, asperiores veniam exercitationem voluptatibus non nesciunt iure.</h3>
+        ---------------------------
+        <SingleComponent />
+        ---------------------------
+        </div>
+</template>
+
+<style lang="css" scoped>
+    h1{
+        color:rgba(202, 23, 23, 0.745);
+    }
+</style>
